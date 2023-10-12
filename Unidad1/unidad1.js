fef751548eb6622181fbd45692db7265867d0d1e
arielxct <campostula.gmail.com>
2023-10-12 13:14:39 -0300

    Primer commit y push de Ejercicio 1

---
+++ Unidad1/unidad1.js
@@ -0,0 +1,46 @@
+/*
+Guía de ejercicios
+Consignas para la realización y entrega de la guía de ejercicios:
+● Todos los ejercicios deben ser realizados siguiendo las pautas que indique cada 
+unidad.
+● Para la entrega de la presente guía, todos los ejercicios deben ser entregados 
+mediante Link de Github y compartidos en el foro del aula virtual
+Unidad 1
+Resolver los ejercicios utilizando variables y operadores. Para el ingreso y egreso de 
+información por el momento con datos propios. 
+1.Realice un algoritmo que muestre el nombre y apellido de un usuario
+2. Realice un algoritmo que muestre el promedio de 3 notas
+3. Realice el algoritmo para calcular e informar el perímetro del triángulo.
+4. Realice el algoritmo para calcular e informar el perímetro y el área del cuadrado.
+5. Realice el algoritmo para calcular e informar el perímetro y el área del rectángulo.
+6. Realice el algoritmo para calcular e informar el resto de la división entera entre los 
+valores.
+7. Realice el algoritmo que arme y muestre el mensaje "Bienvenido", seguido del 
+nombre y apellido del usuario.
+8. Realice el algoritmo para calcular e informar el perímetro y el área del círculo. Se 
+considerará para este ejercicio que el valor de PI será 3.1416.
+*/
+
+// 1.Realice un algoritmo que muestre el nombre y apellido de un usuario
+
+var nombre = "Ariel";
+var apellido = "Campos Tula"
+
+console.log("Mi nombre es : " + nombre + " y mi apellido: " + apellido);
+
+// 2. Realice un algoritmo que muestre el promedio de 3 notas
+
+var nota1 = 6;
+var nota2 = 7;
+var nota3 = 9;
+
+promedio = (nota1+nota2+nota3)/3;
+
+console.log ("El promedio de las tres notas es : " + promedio);
+
+// 3. Realice el algoritmo para calcular e informar el perímetro del triángulo.
+
+
+
+
+

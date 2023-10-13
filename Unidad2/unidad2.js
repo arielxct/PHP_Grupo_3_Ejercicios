6b80e4367649fbd49041d287b122cf2dcabc414f
arielxct <campostula.gmail.com>
2023-10-13 00:10:06 -0300

    Todo los archivos de las unidades ya estan creados, falta resolver las unidades 2 y 3

---
+++ Unidad2/unidad2.js
@@ -0,0 +1,37 @@
+/*
+Unidad 2
+
+9. Se le solicita al usuario que ingrese su nombre y luego su apellido. Realice un
+programa que arme y muestre el mensaje "Bienvenido", seguido del nombre y apellido
+del usuario.
+10. Se le solicita al usuario que ingrese el diámetro de un círculo. Realice un programa
+para calcular e informar el perímetro y el área del círculo. Se considerará para este
+ejercicio que el valor de PI será 3.1416.
+11. Se le solicita al usuario que ingrese 4 números. Realice un programa para calcular e
+informar la suma y el promedio.
+
+Materia: Lógica de programación Profesor: José Alejandro zapata
+12. Realizar un sistema para el cálculo de sueldos de una empresa. Se le solicita que
+ingrese el valor de la hora y la cantidad de horas trabajadas en el mes. Realice un
+programa para calcular e informar el sueldo mensual del empleado.
+13. Realizar un sistema para el cálculo de sueldos de una empresa. Se le solicita que
+ingrese el valor de la hora, la cantidad de horas trabajadas en el mes y los años de
+antigüedad en la empresa. Realice un programa para calcular e informar el sueldo
+mensual del empleado, teniendo en cuenta que se le asigna un bono mensual igual al
+15% del sueldo por año de antigüedad.
+14. Realizar un sistema para el cálculo de sueldo de una empresa de seguros. Se le
+solicita que ingrese el valor de la hora, la cantidad de horas trabajadas en el mes, los
+años de antigüedad, la cantidad de seguros vendidos y el valor del seguro más caro
+vendido. Realice un programa para calcular e informar el sueldo mensual del
+empleado, teniendo en cuenta que se le asigna un bono igual al 50% del seguro más
+caro vendido, un bono igual al 25% del sueldo por la cantidad de seguros vendidos y
+otro bono igual al 15% del sueldo por año de antigüedad. Una vez obtenido el sueldo
+final, calcular e informar el valor promedio de la hora del empleado.
+
+*/
+
+//=================================================================================
+
+// 9. Se le solicita al usuario que ingrese su nombre y luego su apellido. Realice un
+// programa que arme y muestre el mensaje "Bienvenido", seguido del nombre y apellido
+// del usuario.
---
+++ Unidad3/unidad3.js
@@ -0,0 +1,34 @@
+/*
+Unidad 3
+
+15. Se le solicita al usuario que ingrese un número. Realice un programa para informar
+si el número es cero, par o impar.
+16. Se le solicita al usuario que ingrese un número. Realice un programa para informar
+si el número es múltiplo de 3.
+17. Se le solicita al usuario que ingrese una letra. Realice el algoritmo para informar si
+el valor ingresado es una vocal.
+18. Se le solicita al usuario que ingrese los extremos de un rango numérico y un
+número. Realice un programa para informar si el número está dentro del rango.
+Si está en rango, informar si el número es par.
+Si no está dentro del rango, informar si es número es impar.
+Tenga en cuenta que el rango debe tener una diferencia mínima de 5 números
+enteros.
+19. Se le solicita al usuario que ingrese dos números y un operador (+, -, *, /). Realice
+un programa para calcular e informar la operación solicitada entre ambos números.
+20. Se le solicita al usuario que ingrese los tres lados de un triángulo. Realice el
+algoritmo para informar si el triángulo es equilátero, isósceles o escaleno.
+21. Realizar un sistema para el cálculo de sueldo de una empresa. Se le solicita al
+usuario que ingrese la categoría a la que pertenece que define el sueldo básico
+mensual (categoría 1: u$s 2000, categoría 2: u$s 3000, categoría 3: u$s 4000) y las
+horas extras. Se sabe que:
+
+Materia: Lógica de programación Profesor: José Alejandro zapata
+a. Si es de la categoría 1 y trabajó más de 20 horas extra, suma un bono de u$s 500
+más.
+b. Si es de la categoría 3 y trabajó más de 30 horas extra, suma un bono de u$s 1000
+más.
+Realice un programa para calcular e informar el sueldo mensual del empleado,
+indicando si su sueldo supera o no los u$s 4000.
+*/
+
+//======================================================================
